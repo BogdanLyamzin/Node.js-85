c621f7a170938768f61000a8a596d540a3d5d113
unknown <bogdan.lyamzin.d@gmail.com>
2023-10-31 20:15:27 +0200

    add project-example to lesson 1

---
+++ lesson-1/node start/index.js
@@ -0,0 +1,3 @@
+const now = new Date();
+
+console.log(`Today ${now.getFullYear()} year`);
---
+++ lesson-1/node-CommonJS-module/users.js
@@ -0,0 +1,12 @@
+const admins = ["Alex", "Andrey", "VAsiliy"];
+
+const clients = ["Anna", "Alina", "Tamara"];
+
+const users = {
+    admins,
+    clients
+};
+
+module.exports = users;
+
+
