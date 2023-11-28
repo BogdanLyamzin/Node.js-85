657c5e0fb92cd96cf8172f1300125162633546fd
unknown <bogdan.lyamzin.d@gmail.com>
2023-11-28 21:04:35 +0200

    finish lesson 9

---
+++ lesson-9/cloudinary-example/helpers/cloudinary.js
@@ -0,0 +1,11 @@
+import {v2 as cloudinary} from "cloudinary";
+
+const {CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} = process.env;
+
+cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+})
+
+export default cloudinary;
